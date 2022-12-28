d33ba057fc15ca60bd00594dfcdeed264ab3338a
Kucman <kucera.j@atlas.cz>
2022-12-28 09:37:06 +0000

    finished working on fields. Now need to tie everything up in the simulation

--- simulation_GUI/fields.py
+++ simulation_GUI/fields.py
@@ -25,11 +25,12 @@ class Field(ABC):
     # detuning is calculated as delta = w - w0 = w - E_high + E_low
     # some fields shift only E_low, some fields shift both
     @abstractmethod
-    def calculate_detuning(self, positions, time, cloud):
+    def calculate_detuning(self, positions, momenta, time, cloud):
         pass
     
+    # this is due to resonnant and off resonant scattering events
     @abstractmethod
-    def calculate_scattering_momenta(self, positions, time, delta_t, cloud):
+    def calculate_scattering_momenta(self, positions, momenta, B_field_direction, local_detunings, time, delta_t, cloud):
         pass
     
     
@@ -42,21 +43,44 @@ class ResonantField(Field):
     def add_beam(self, beam):
         self.resonant_beams.append(beam)
         
+    # resonant beams dont exert a conservative force
     def calculate_force(self, positions, time, cloud):
-        pass
-    
-    def calculate_detuning(self, positions, time, cloud):
         return np.zeros(positions.shape)
     
-    def calculate_scattering_momenta(self, positions, time, delta_t, cloud):
-        pass
+    # resonant beams dont cause local detunings
+    def calculate_detuning(self, positions, momenta, time, cloud):
+        return np.zeros(positions.shape)
     
+    def calculate_scattering_momenta(self, positions, momenta, B_field_direction, local_detunings, time, delta_t, cloud):
+        # first calculate saturation parameters
+        saturation_params = []
+        for resonant_beam in self.resonant_beams:
+            saturation_params.append(resonant_beam.calculate_saturation_parameter(positions, B_field_direction, time))
+        
+        combined_saturation = np.zeros(len(positions))
+        for s in saturation_params:
+            combined_saturation = combined_saturation + s
+        
+        
+        scattering_momenta = np.zeros(positions.shape)
+        for i, resonant_beam in enumerate(self.resonant_beams):
+            s = saturation_params[i]
+            # beam detuning + local detuning + doppler detuning 
+            detuning = resonant_beam.detuning_ramp.get_value(time) + resonant_beam.calculate_doppler_detuning(momenta / cloud.particle_mass) + local_detunings
+            fraction = s / (1 + combined_saturation + 4 * (detuning**2) / (resonant_beam.transition_linewidth**2))
+            number_of_scattering_events = delta_t * resonant_beam.transition_linewidth / 2 * fraction
+            scattering_momenta = scattering_momenta + resonant_beam.calculate_scattering_momenta(number_of_scattering_events)
+        return scattering_momenta
+    
+# TODO check how polarisation is defined
 class ResonantBeam():
-    def __init__(self, detuning_ramp = Ramp(), intensity_ramp = Ramp(), beam_direction = np.array([1,0,0]), polarisation = -1):
+    def __init__(self, detuning_ramp, power_ramp, beam_direction, polarisation = -1, waist = 2.5E-2):
         self.detuning_ramp = detuning_ramp
-        self.intensity_ramp = intensity_ramp
-        self.polarisation = polarisation
+        self.power_ramp = power_ramp
+        # direction is taken to be in the lab frame and needs to be a unit vector
         self.beam_direction = beam_direction
+        self.polarisation = polarisation
+        self.waist = waist
         
         ''' constant parameters for a given transition '''
         self.wavelength = 582.84E-9                 # m
@@ -64,15 +88,42 @@ class ResonantBeam():
         self.transition_linewidth = 2*np.pi*190*1E3 # rad /s
         self.dipole_element_squared = self.transition_linewidth * 3 * np.pi * scipy.constants.epsilon_0 * \
                                       scipy.constants.hbar * (self.wavelength / (2 * np.pi))**3
-                                      
-        
-    #TODO implement detuning calculation (probably doppler)
-    #TODO implement scattering heating calculation
-    #TODO implement scattering force with the knowledge of detuning
-    #TODO check that the heating is correct
-    #TODO somehow implement saturation from other beams
-        
     
+    def calculate_beam_intensity(self, positions, time):
+        # need to find the radial distance from the center of the beam
+        projection = np.dot(positions, self.beam_direction).reshape(len(positions), 1)
+        radial_component = positions - projection * self.beam_direction
+        radial_distance = np.sqrt((radial_component * radial_component).sum(axis = 1))
+        
+        intensity = self.power_ramp.get_value(time) * (2 / np.pi) * (1 / (self.waist)**2) * \
+                    np.exp(- 2 * np.power(radial_distance, 2) / np.power(self.waist, 2))
+        return intensity
+    
+    def calculate_scattering_momenta(self, number_of_scattering_events):
+        # impair momentum from photons along the beam and then from random re-emitted photons
+        photon_momentum = scipy.constants.hbar * (2 * np.pi / self.wavelength)
+        emission_momentum_magnitude =  photon_momentum * np.sqrt(number_of_scattering_events)
+        random_phi = np.random.rand(len(number_of_scattering_events)) * 2 * np.pi
+        random_theta = np.arccos(1 - 2 * np.random.rand(len(number_of_scattering_events)))
+        emission_momenta = emission_momentum_magnitude.reshape((len(number_of_scattering_events),1)) * \
+                           np.stack((np.sin(random_theta)*np.sin(random_phi), np.sin(random_theta)*np.cos(random_phi), np.cos(random_theta)), axis=1)
+        absorption_momenta = photon_momentum * number_of_scattering_events.reshape((len(number_of_scattering_events), 1)) * self.beam_direction
+        return absorption_momenta + emission_momenta
+    
+    def calculate_doppler_detuning(self, velocities):
+        return - 2 * np.pi / (self.wavelength) * np.dot(velocities, self.beam_direction)
+        
+    def calculate_saturation_parameter(self, positions, B_field_direction, time):
+        # projection of beam component along the field
+        cos_B_k = np.dot(B_field_direction, self.beam_direction)
+        beam_intensity = self.calculate_beam_intensity(positions, time)
+        
+        s_prefactor = 4 / (scipy.constants.c * scipy.constants.epsilon_0 * (scipy.constants.hbar * self.transition_linewidth)**2) * beam_intensity
+        s_dipole = self.dipole_element_squared * ((1 - cos_B_k * self.polarisation)**2) / 4 
+        return s_prefactor * s_dipole
+        
+        
+        
 #%% DIPOLAR FIELD
     
 class DipoleField(Field):
@@ -91,14 +142,14 @@ class DipoleField(Field):
     
     # detuning is calculated as delta = w - w0 = w - E_high + E_low
     # delE_high = 0 so we just add the change in the low level (which is negative)
-    def calculate_detuning(self, positions, time, cloud):
+    def calculate_detuning(self, positions, momenta, time, cloud):
         detuning = np.zeros(len(positions))
         # bottom level shifts down so detuning is reduced
         for dipole_beam in self.dipole_beams:
             detuning = detuning + cloud.intensity_prefactor * dipole_beam.calculate_beam_intensity(positions, time)
         return detuning
     
-    def calcualte_scattering_momenta(self, positions, time, delta_t, cloud):
+    def calcualte_scattering_momenta(self, positions, momenta, B_field_direction, local_detunings, time, delta_t, cloud):
         scattering_momenta = np.zeros(positions.shape)
         for dipole_beam in self.dipole_beams:
             number_of_scattering_events = delta_t * cloud.scattering_rate * dipole_beam.calculate_beam_intensity(positions, time)
@@ -267,7 +318,7 @@ class MagneticField(Field):
     
     # detuning is delta = w - E_high + E_low
     # E = - mu * mag(B)
-    def calculate_detuning(self, positions, time, cloud):
+    def calculate_detuning(self, positions, momenta, time, cloud):
         field = self.uniform_field.calculate_field(positions, time) + self.gradient_field.calculate_field(positions, time)
         field_magnitude = np.sqrt((field * field).sum(axis = 1))
         return field_magnitude * (- cloud.get_ground_state_moment() + cloud.get_excited_state_moment())
@@ -283,5 +334,5 @@ class MagneticField(Field):
         
         return cloud.get_ground_state_moment() * np.stack((magnitude_gradient_x, magnitude_gradient_y, magnitude_gradient_z), axis=1)
         
-    def calculate_scattering_momenta(self, positions, time, cloud):
+    def calculate_scattering_momenta(self, positions, momenta, B_field_direction, local_detunings, time, delta_t, cloud):
         return np.zeros(positions.shape)
